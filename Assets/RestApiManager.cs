b54cc12b093b2975dc19a20d187403cd01f86082
Juan José <67957125+JJaoMein@users.noreply.github.com>
2023-02-07 18:53:58 -0500

    Puto Json

--- Assets/RestApiManager.cs
+++ Assets/RestApiManager.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using static System.Net.WebRequestMethods;
 
 public class RestApiManager : MonoBehaviour
 {
 
-    [SerializeField] string rickAndMortyApi = "https://rickandmortyapi.com/api/character/";
-    [SerializeField] string myApi;
-    [SerializeField] int characterId = 1;
+    //[SerializeField] string rickAndMortyApi = "https://rickandmortyapi.com/api/character/";
+    //public string myApi = "https://my-json-server.typicode.com/JJaoMein/TempJSONServer/users/";
+    //[SerializeField] int characterId = 1;
     [SerializeField] RawImage yourRawImage;
 
     void Start()
@@ -19,74 +20,69 @@ public class RestApiManager : MonoBehaviour
 
     public void ActionOnCLick()
     {
-        StartCoroutine(GetText());
+        StartCoroutine(GetUsers());
     }
 
-    //IEnumerator GetUsers()
-    //{
-    //    UnityWebRequest www = UnityWebRequest.Get(myApi);
-    //    yield return www.SendWebRequest();
 
-    //    if (www.result == UnityWebRequest.Result.ConnectionError)
-    //    {
-    //        Debug.Log(www.error);
-    //    }
-    //    else
-    //    {
-    //        // Show results as text
-    //        Debug.Log(www.downloadHandler.text);
-
-    //        if (www.responseCode == 200)
-    //        {
-    //            User users = JsonUtility.FromJson<User>(www.downloadHandler.text);
-
-    //            foreach (int id in users.id)
-    //            {
-    //                Debug.LogWarning("Name: " + users.name);
-
-    //            }
-    //        }
-    //        else
-    //        {
-    //            string mensaje = www.responseCode.ToString();
-    //            mensaje += "\ncontent-type" + www.GetRequestHeader("content-type");
-    //            mensaje += "\nError: " + www.error;
-    //            Debug.Log(mensaje);
-    //        }
-    //    }
-    //}
-
-    IEnumerator GetText()
+    IEnumerator GetUsers()
     {
-        UnityWebRequest www = UnityWebRequest.Get(rickAndMortyApi);
+        UnityWebRequest www = UnityWebRequest.Get("https://my-json-server.typicode.com/JJaoMein/TempJSONServer/users");
         yield return www.SendWebRequest();
 
         if (www.result == UnityWebRequest.Result.ConnectionError)
         {
-            Debug.Log(www.error);
+            Debug.Log("ESTE ERROR" + www.error);
         }
         else
         {
-            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+            string json = www.downloadHandler.text;
+
+            if (www.responseCode == 200)
+            {
+                Debug.Log("Hola");
+                UserJsonData myUser = JsonUtility.FromJson<UserJsonData>(json);
+            }
+            else
+            {
+                string mensaje = www.responseCode.ToString();
+                mensaje += "\ncontent-type" + www.GetRequestHeader("content-type");
+                mensaje += "\nError!!!: " + www.error;
+                Debug.Log(mensaje);
+            }
+        }
+    }
+
+    IEnumerator GetCharacter()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://rickandmortyapi.com/api/character");
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log("ESTE ERROR" + www.error);
+        }
+        else
+        {
             Debug.Log(www.downloadHandler.text);
 
             if (www.responseCode == 200)
             {
+
                 CharacterList characters = JsonUtility.FromJson<CharacterList>(www.downloadHandler.text);
 
                 foreach (Character character in characters.results)
                 {
-                    Debug.LogWarning("Name: " + character.name);
+                    Debug.Log("Name: " + character.name);
                     Debug.Log("Image: " + character.image);
                     StartCoroutine(GetImage(character.image));
-                    break;
                 }
             }
             else
             {
                 string mensaje = www.responseCode.ToString();
                 mensaje += "\ncontent-type" + www.GetRequestHeader("content-type");
-                mensaje += "\nError: " + www.error;
+                mensaje += "\nError!!!: " + www.error;
                 Debug.Log(mensaje);
             }
         }
@@ -108,10 +104,12 @@ public class RestApiManager : MonoBehaviour
 }
 
 
-public class User
+[System.Serializable]
+public class UserJsonData
 {
     public int id;
     public string name;
+    public int[] deck;
 }
 
 [System.Serializable]
